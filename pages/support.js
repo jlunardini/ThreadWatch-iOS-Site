8903b22f0bb0bcaf6c3f2aa87e856dfaeba579ee
Johncarlos Lunardini <john-carlos.lunardini@mikestechshop.com>
2023-11-06 19:24:12 -0500

    flesh out privacy and support pages

--- pages/support.js
+++ pages/support.js
@@ -1,22 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Support() {
   return (
     <main className="flex min-h-screen flex-col md:flex-row gap-12 items-center md:justify-center md:p-24 bg-[#0E0E0E] overflow-y-hidden relative">
-      <div className="flex flex-col pt-24 px-24 md:p-0 flex-shrink">
-        <h1 className="text-primaryOrange text-5xl font-DankMono">
-          ThreadWatch
-        </h1>
-        {/* {<h2 className="text-[#9D9D9D] text-3xl mt-3">Sweet fades brah.</h2>} */}
-        <ul className="list-inside list-disc text-primaryGray mt-4 text-lg font-sans">
-          <li>Catalogue all your raw denim</li>
-          <li>Track wears and washes</li>
-          <li>Add fade pics to see progress over time</li>
-        </ul>
-        <div className="mt-6 self-start font-DankMono text-primaryOrange px-4 py-1 rounded-full border border-primaryOrange">
-          Coming October 2023
-        </div>
+      <div className="flex flex-col pt-24 px-24 md:p-0 flex-shrink lg:max-w-md">
+        <h1 className="text-primaryOrange text-5xl font-DankMono">Support</h1>
+        <p className="text-primaryGray mt-4">
+          For any and all issues email{" "}
+          <a
+            className="underline text-primaryOrange decoration-primaryOrange"
+            href="mailto:jlunardini@hey.com"
+          >
+            the developer
+          </a>
+        </p>
       </div>
       <img
         alt="Screenshot of ThreadWatch iOS App"
